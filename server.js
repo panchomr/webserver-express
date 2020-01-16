100de7f1b281dd01027292316d2b481475743c36
Francisco <fmunoz@servel.cl>
2020-01-16 10:49:59 -0300

    primer commit

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+
+require('./hbs/helpers/helpers');
+
+// obtenber puerto para despliegue en heroku
+
+const port = process.env.PORT || 3000;
+
+
+
+app.use(express.static(__dirname + '/public'));
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+
+
+
+
+app.get('/', function(req, res) {
+
+    res.render('home', {
+        nombre: 'Francisco',
+        anio: new Date().getFullYear()
+    });
+});
+
+
+app.get('/about', function(req, res) {
+
+    res.render('about', {
+        anio: new Date().getFullYear()
+    });
+});
+
+// app.get('/about', function(req, res) {
+
+//     res.render('about', {
+//         anio: new Date().getFullYear()
+//     });
+// });
+
+app.listen(port, () => {
+    console.log(`escuchando el puerto ${port}`);
+});
